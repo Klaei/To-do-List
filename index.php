99dfaeeafce7acf4b753e599b81a7d014868f252
Brian Chato <estipularbrian@gmail.com>
2025-07-13 09:41:43 +0800

    latest push 2

--- index.php
+++ index.php
@@ -17,48 +17,73 @@ include 'config.php';
     </style>
 
 </head>
-    
+
 <body>
-        <div class="container my-4">
-            <div class="row">
-                <div class="col-sm">
-                    <div class="card px-5" style="background-color:#234465;">
-                        <div class="card-body">
-                            <h3 class="text-center text-white">To Do List</h3>
-                            <div class="d-flex justify-content-end">
-                                <button type="submit" class="btn btn-primary" name="Add" add data-bs-toggle="modal" data-bs-target="#inputtask">Add to list</button>
-                                <!--------------------------------------modal form ---------------------------------------------------->
-                                <div class="modal fade" id="inputtask" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <!--------------------------------------main form ---------------------------------------------------->
-                                                <form action="#" method="POST">
-                                                    <div class="d-flex justify-content-end">
-                                                        <input class="form-control" type="text" id="taskID" style="width:5rem;"disabled readonly>
-                                                    </div>
-                                                    <div class="mb-3">
-                                                        <label for="task">Enter task you want to add:</label>
-                                                        <textarea type="text" class="form-control" id="task" rows="3"></textarea>
-                                                    </div>
-                                                    <input type="submit" class="btn btn-primary" value="Submit" onclick='alert("Task Added")';>
-                                                </form>
-                                                <!--------------------------------------end of main form ---------------------------------------------------->
-                                            </div>
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card px-5" style="background-color:#234465;">
+                    <div class="card-body">
+                        <h3 class="text-center text-white">To Do List</h3>
+                        <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary" name="Add" data-bs-toggle="modal"
+                                data-bs-target="#inputtask">Add to list</button>
+                            <!--------------------------------------modal form ---------------------------------------------------->
+                            <div class="modal fade" id="inputtask" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!--------------------------------------main form ---------------------------------------------------->
+                                            <form action="#" method="POST">
+                                                <div class="d-flex justify-content-end">
+                                                    <input class="form-control" type="text" id="taskID"
+                                                        style="width:5rem;" readonly>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="task">Enter task you want to add:</label>
+                                                    <textarea type="text" class="form-control" name="task" id="task"
+                                                        rows="3"></textarea>
+                                                </div>
+                                                <input type="submit" class="btn btn-primary" value="Submit"
+                                                    onclick='alert("Task Added")'>
+                                            </form>
+                                            <!--------------------------------------end of main form ---------------------------------------------------->
                                         </div>
                                     </div>
                                 </div>
-                                <!--------------------------------------end of modal form ---------------------------------------------------->
+                            </div>
+                            <!--------------------------------------end of modal form ---------------------------------------------------->
+                        </div>
+                        <!-------------------------------------- Start of Task Table ---------------------------------------------------->
+                        <div class="container mt-3">
+                            <div class="row">
+                                <div class="col">
+                                    <table class="table table-striped text-white text-center">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Task</th>
+                                                <th>Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
+                        <!--------------------------------------end of task Table ---------------------------------------------------->
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
